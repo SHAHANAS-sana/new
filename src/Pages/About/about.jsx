790b5f9c3570e309b52275e6ae9fde9b38205f50
Shahanas <sanashahanas67@gmail.com>
2025-10-18 19:38:15 +0530

    Initial commit

---
+++ src/Pages/About/about.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import './about.css';
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+export default About;
---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,10 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+import './home.css';
+
+const Home = () => {
+  const navigate = useNavigate();
+  return <img src="bgreen.jpeg" alt="" />;
+};
+
+export default Home;
